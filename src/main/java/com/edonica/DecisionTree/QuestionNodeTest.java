341c8ee3a2e2c165cb8902530b6da5ba41860e79
Ed Goward <egoward@microsoft.com>
2016-10-05 21:16:08 +0100

    More refactoring

---
+++ src/main/java/com/edonica/DecisionTree/QuestionNodeTest.java
@@ -0,0 +1,24 @@
+package com.edonica.DecisionTree;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by edward on 05/10/2016.
+ */
+public class QuestionNodeTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void test1() {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+}
